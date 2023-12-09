e67c4c81637e1916331a96aad5c3f74c5eedfa7a
Ruizerick26 <ruizerick2525@gmail.com>
2023-12-08 20:50:54 -0500

    funcion multiplicar

---
+++ src/CalcuE/Testcalculadora.java
@@ -0,0 +1,8 @@
+package CalcuE;
+
+public class Testcalculadora {
+	public static void main(String[] args) {
+		Calculadora MiC = new Calculadora();
+		System.out.println("Resultado de multiplicación: "+ MiC.multiplicar(2, 5)); 
+	}
+}
